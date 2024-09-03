f7450a55e09ac0cec2f3b65ffaa9f3e562a8113e
prasad <prasadladhane123@gmail.com>
2024-09-03 20:26:03 +0530

    variables and methods dailycodes

---
+++ dailycodes/variables-methods/Program9.dart
@@ -0,0 +1,15 @@
+class Player{
+    int? jerNo;
+    String? pName;
+    static String? country="India";
+    Player(this.jerNo,this.pName);
+
+    static void playerInfo(){
+        print(jerNo);
+        print(pName);
+        print(country);
+    }
+}
+void main(){
+    Player.playerInfo();
+}

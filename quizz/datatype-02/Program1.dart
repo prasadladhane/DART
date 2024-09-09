daa88e2ba25039761d6dd06d32cd0ce0458ddbd2
prasad <prasadladhane123@gmail.com>
2024-09-09 13:02:53 +0530

    datatype-02-quizz

---
+++ quizz/datatype-02/Program1.dart
@@ -0,0 +1,10 @@
+void main(){
+    dynamic x=10;
+    print(x.runtimeType);//int
+
+    x=10.6;
+    print(x.runtimeType);//double
+
+    x="Incubator";
+    print(x.runtimeType);//String
+}
---
+++ quizz/datatype-02/Program3.dart
@@ -0,0 +1,6 @@
+void main(){
+    const int x=10;
+    const int y;
+    print(x);
+    print(y);
+}

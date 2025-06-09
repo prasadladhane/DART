9762889bcd4b44a7bd864c3f23a5b25186afaa93
prasad <prasadladhane123@gmail.com>
2025-06-09 19:08:44 +0530

    finished function revision

---
+++ dailycodes/functions/Program10.dart
@@ -0,0 +1,8 @@
+void greet(String name){
+    print(name);
+}
+
+void main(){
+//    greet('Prasad');
+      greet();
+}
---
+++ dailycodes/functions/Program11.dart
@@ -0,0 +1,6 @@
+void greet({String? name,String? age}){
+    print("My name is: $name My age is:$age");
+}
+void main(){
+    greet(name:"Prasad",age:"22");
+}

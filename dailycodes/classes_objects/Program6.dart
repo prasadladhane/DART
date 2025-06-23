7349b1e5a0ecf2d499a7c22fb2464811ab4835de
prasad <prasadladhane123@gmail.com>
2025-06-23 21:09:14 +0530

    classes and object revision

---
+++ dailycodes/classes_objects/Program6.dart
@@ -0,0 +1,22 @@
+class Demo{
+    int x=10;
+    String name="Prasad";
+    
+    void fun(){
+        print(x);
+        print(name);
+    }
+}
+class Memo{
+    int x=20;
+    String name="Ladhane";
+    
+    void run(){
+        print(x);
+        print(name);
+    }
+}
+void main(){
+    Demo obj1=Demo();
+    obj1.fun();    
+}

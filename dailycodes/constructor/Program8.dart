20009527f617247f3b231bd29798a8208bbceef6
prasad <prasadladhane123@gmail.com>
2025-06-23 20:44:20 +0530

    constructor revision

---
+++ dailycodes/constructor/Program8.dart
@@ -0,0 +1,13 @@
+class Parent{
+    int jerNo=18;
+    String pName="Virat";
+    
+    Parent(this.jerNo,this.pName){
+        print(pName);
+        print(jerNo);
+    }
+}
+void main(){
+    Parent obj=Parent(45,"Rohit");
+}
+

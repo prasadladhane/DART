76ceb3aff852d83b96da88082c01b912abc593f6
prasad <prasadladhane123@gmail.com>
2026-03-11 21:51:19 +0530

    getter revision

---
+++ dailycodes/encapsulation/setter-getter/Program3.dart
@@ -0,0 +1,7 @@
+class Person{
+    String name="Prasad";
+
+    String getName(){
+        return name;
+    }
+}
---
+++ dailycodes/encapsulation/setter-getter/Program4.dart
@@ -0,0 +1,7 @@
+import 'Program3.dart';
+
+void main(){
+    Person obj=Person();
+    String name=obj.getName();
+    print(name);
+}
